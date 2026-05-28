5f32d150619c22ced44ed59e4aa49ad0a4a8df4e
Maxi <m.lopez@itecriocuarto.org.ar>
2026-05-28 16:44:15 -0300

    modifique el register y algo de visualizacion

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -7,3 +7,20 @@ class UsuarioPersonalizadoForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = UsuarioPersonalizado
         fields = UserCreationForm.Meta.fields + ('telefono','dni','foto_perfil')
+
+class UsuarioRegistroForm(UserCreationForm):
+
+    class Meta:
+
+        model = UsuarioPersonalizado
+
+        fields = [
+            'username',
+            'email',
+            'telefono',
+            'dni',
+            'fecha_nacimiento',
+            'foto_perfil',
+            'password1',
+            'password2',
+        ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,61 @@
 from django.shortcuts import render, redirect
-from .forms import UsuarioPersonalizadoForm
+from .forms import UsuarioPersonalizadoForm, UsuarioRegistroForm
+from django.contrib.auth import login
+
+
 # Create your views here.
+
 def nuevo_usuarios(request):
+
     if request.method == "POST":
+
         print(request.FILES)
-        form = UsuarioPersonalizadoForm(request.POST, request.FILES)
+
+        form = UsuarioPersonalizadoForm(
+            request.POST,
+            request.FILES
+        )
+
         if form.is_valid():
+
             form.save()
+
             return redirect('usuarios')
+
     else:
-        form= UsuarioPersonalizadoForm()
-    return render(request,'usuarios/usuarios.html',{'form':form})
+
+        form = UsuarioPersonalizadoForm()
+
+    return render(
+        request,
+        'usuarios/usuarios.html',
+        {'form': form}
+    )
+
+
+def register(request):
+
+    if request.method == 'POST':
+
+        form = UsuarioRegistroForm(
+            request.POST,
+            request.FILES
+        )
+
+        if form.is_valid():
+
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('rutinas')
+
+    else:
+
+        form = UsuarioRegistroForm()
+
+    return render(
+        request,
+        'usuarios/register.html',
+        {'form': form}
+    )
