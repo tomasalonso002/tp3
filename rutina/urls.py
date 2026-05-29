a65c904b066eec015cb118ec079a4b1b44fa08aa
Maxi <m.lopez@itecriocuarto.org.ar>
2026-05-29 17:40:47 -0300

    Conflictos en rutina/urls.py resueltos manteniendo el CRUD completo

--- rutina/urls.py
+++ rutina/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.rutinas, name='rutinas'), 
     path('crear/', views.crear_rutina, name='crear_rutina'),
     path('editar/<int:id>/', views.editar_rutina, name='editar_rutina'),
     path('<int:id>/eliminar/', views.eliminar_rutina, name='eliminar_rutina'),
-=======
-    path('rutinas/', views.crear_rutina, name='crear_rutina'),
->>>>>>> main
 ]
